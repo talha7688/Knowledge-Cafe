da6169013f79ed3ad82fbb5194627b7900f5a200
unknown <talha7688>
2023-10-06 01:10:07 +0600

    added json fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
-      <h1 className='text-5xl text-blue-500'>Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-4xl">Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
